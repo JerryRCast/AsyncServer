ae7a205b1186ac58f22c8bc8edf4e29eed61f416
Gerardo Rios <gerardo.rios0194@gmail.com>
2018-09-13 16:47:19 -0500

    V1.0
    Servidor asíncrono, plantilla, básico, solo recibe, no responde

---
+++ ServerMultiThread/ServerMultiThread/ClientState.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Sockets;
+
+namespace ServerMultiThread
+{
+    public class ClientState
+    {
+        public TcpClient client;
+        public byte[] buffer;
+    }
+}
---
+++ ServerMultiThread/ServerMultiThread/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ServerMultiThread
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            /*Console.WriteLine("tcplistener by hvivani – 20110225 – para el negro: \r\n");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Parametros: Puerto IP   \r\n");
+                Console.WriteLine("Ejemplo: tcplistener 2525 127.0.0.1  \r\n");
+                Console.WriteLine("si tenes mas de una interfaz de red, usa la ip de dicha interfaz.  \r\n");
+                Console.ReadKey();
+                return 1;
+            }
+            else
+            {*/
+            Console.WriteLine("Iniciando.");
+            multicnnNew.iniciar("99","127.0.0.1");//(args[0], args[1]);
+                return 0;
+            //}
+        }
+    }
+}
